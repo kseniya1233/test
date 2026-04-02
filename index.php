fc268d0d947aa1e65b746a8ea385b9f234c1872b
Ксения <kseniyasemashko7777@mail.ru>
2026-04-02 12:02:42 +0300

    lab13: задание 3 - созданы 2 объекта и установлены свойства

--- index.php
+++ index.php
@@ -16,5 +16,19 @@ class Worker
     public $salary;
 }
 
+$worker1 = new Worker();
+$worker1->name = "Иван";
+$worker1->age = 25;
+$worker1->salary = 50000;
+
+$worker2 = new Worker();
+$worker2->name = "Мария";
+$worker2->age = 30;
+$worker2->salary = 60000;
+
+echo "<h2>Информация о работниках</h2>";
+echo "Работник 1: " . $worker1->name . ", возраст: " . $worker1->age . ", зарплата: " . $worker1->salary . "<br>";
+echo "Работник 2: " . $worker2->name . ", возраст: " . $worker2->age . ", зарплата: " . $worker2->salary . "<br>";
+
 echo "</body></html>";
 ?>
