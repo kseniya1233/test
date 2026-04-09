10593e1f183178a2ec955d6bf1293d2921464736
Ксения <kseniyasemashko7777@mail.ru>
2026-04-09 11:52:28 +0300

    lab14: задание 5 - добавлены страницы Италии и Японии

--- index.php
+++ index.php
@@ -12,6 +12,8 @@ echo "<!DOCTYPE html>
         .date { color: #666; font-size: 12px; }
         .card img { width: 100%; height: 150px; object-fit: cover; border-radius: 5px; }
         .card a { text-decoration: none; color: inherit; display: block; }
+        .back-link { margin-top: 20px; display: inline-block; }
+        .detail-img { width: 100%; max-width: 500px; border-radius: 10px; margin-bottom: 20px; }
         nav { margin-bottom: 20px; }
         nav a { margin-right: 15px; }
     </style>
@@ -55,10 +57,64 @@ class BlogPage extends Page
             </a>
         </div>
     </div>";
+    
+    public function render(): void
+    {
+        echo $this->template;
+    }
 }
 
-$page = new BlogPage();
-$page->render();
+class ItalyPage extends Page
+{
+    private string $name = "italy";
+    private string $template = "<div>
+        <img class='detail-img' src='https://avatars.mds.yandex.net/i?id=5a6c9f5e2f03581c33859d314d07ed39_l-5221549-images-thumbs&n=13' alt='Италия'>
+        <h2>Путешествие в Италию</h2>
+        <p>Рим и Флоренция - незабываемые впечатления!</p>
+        <p>Мы посетили Колизей, Пизанскую башню, галерею Уффици.</p>
+        <p>Дата поездки: Июль 2025</p>
+        <a href='?page=page' class='back-link'>← Вернуться на главную</a>
+    </div>";
+    
+    public function render(): void
+    {
+        echo $this->template;
+    }
+}
+
+class JapanPage extends Page
+{
+    private string $name = "japan";
+    private string $template = "<div>
+        <img class='detail-img' src='https://avatars.mds.yandex.net/i?id=08fda78567a2fe3d30a333c41459c981_l-5176581-images-thumbs&n=13' alt='Япония'>
+        <h2>Путешествие в Японию</h2>
+        <p>Токио и Киото - знакомство с японской культурой</p>
+        <p>Мы посетили храм Сэнсодзи, императорский дворец, район гейш в Киото.</p>
+        <p>Дата поездки: Весна 2025</p>
+        <a href='?page=page' class='back-link'>← Вернуться на главную</a>
+    </div>";
+    
+    public function render(): void
+    {
+        echo $this->template;
+    }
+}
+
+$pageParam = $_GET['page'] ?? null;
+
+if ($pageParam === 'italy') {
+    $page = new ItalyPage();
+    $page->render();
+} elseif ($pageParam === 'japan') {
+    $page = new JapanPage();
+    $page->render();
+} elseif ($pageParam === 'blog') {
+    $page = new BlogPage();
+    $page->render();
+} else {
+    $page = new Page();
+    $page->render();
+}
 
 echo "</body></html>";
 ?>
