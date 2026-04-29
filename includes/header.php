ae9938f26922605e7156a034b6e7d9377268daaa
Ксения <kseniyasemashko7777@mail.ru>
2026-04-29 13:25:29 +0300

    Исправлен header

--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,14 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Мой сайт</title>
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
+</head>
+<body>
+
 <?php
 $menuItems = [
     'Главная' => '/index.php',
@@ -11,7 +22,7 @@ $menuItems = [
         <ul>
             <?php foreach ($menuItems as $title => $url): ?>
                 <li>
-                    <a href="<?= $url ?>" <?= $_SERVER['REQUEST_URI'] == $url ? 'class="active"' : '' ?>>
+                    <a href="<?= $url ?>" <?= ($_SERVER['REQUEST_URI'] == $url) ? 'class="active"' : '' ?>>
                         <?= $title ?>
                     </a>
                 </li>
